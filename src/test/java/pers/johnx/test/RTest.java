3751c4ad342f945d14327502cb4f1bd19cc94934
JohnX <xyzdxb@126.com>
2020-10-20 09:30:17 +0800

    Spring 整合 redis

--- src/test/java/pers/johnx/test/RTest.java
+++ src/test/java/pers/johnx/test/RTest.java
@@ -7,6 +7,9 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
+import java.util.Set;
+
 /**
  * @BelongsProject: SpringDataRedisDemo
  * @BelongsPackage: pers.johnx.test
@@ -21,7 +24,7 @@ public class RTest {
     private RedisTemplate redisTemplate;
 
     @Test
-    public void setValue(){
+    public void setValue1(){
         redisTemplate.boundValueOps("name").set("youjiuye");
     }
     @Test
@@ -30,8 +33,64 @@ public class RTest {
         System.out.println(str);
     }
     @Test
-    public void deleteValue(){
+    public void deleteValue1(){
         redisTemplate.delete("name");;
+    }
+
+
+    /**
+     * 存入值
+     */
+    @Test
+    public void setValue2(){
+        redisTemplate.boundSetOps("nameset").add("曹操");
+        redisTemplate.boundSetOps("nameset").add("刘备");
+        redisTemplate.boundSetOps("nameset").add("孙权");
+    }
+
+    /**
+     * 提取值
+     */
+    @Test
+    public void getValue2(){
+        Set members = redisTemplate.boundSetOps("nameset").members();
+        System.out.println(members);
+    }
+
+    /**
+     * 删除集合中的某一个值
+     */
+    @Test
+    public void deleteValue2(){
+        redisTemplate.boundSetOps("nameset").remove("孙权");
+    }
+
+    /**
+     * 删除整个集合
+     */
+    @Test
+    public void deleteAllValue2(){
+        redisTemplate.delete("nameset");
+    }
+
+
+    /**
+     * 右压栈：后添加的对象排在后边
+     */
+    @Test
+    public void testSetValue3(){
+        redisTemplate.boundListOps("namelist1").rightPush("刘备");
+        redisTemplate.boundListOps("namelist1").rightPush("关羽");
+        redisTemplate.boundListOps("namelist1").rightPush("张飞");
+    }
+
+    /**
+     * 显示右压栈集合
+     */
+    @Test
+    public void testGetValue3(){
+        List list = redisTemplate.boundListOps("namelist1").range(0, 10);
+        System.out.println(list);
     }
 
 }
